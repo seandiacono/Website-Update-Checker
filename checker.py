68e30dc252c4c1cfae4c30d6c8f417cde926c522
Sean Diacono <seandiacono@gmail.com>
2022-02-02 16:06:59 +0100

    + Added Docker files

--- checker.py
+++ checker.py
@@ -10,26 +10,27 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
-url = Request('https://www.svenskfotboll.se/biljett/',
-              headers={'User-Agent': 'Mozilla/5.0'})
+while True:
+    url = Request('https://www.svenskfotboll.se/biljett/',
+                  headers={'User-Agent': 'Mozilla/5.0'})
 
-current_response = urlopen(url).read()
+    current_response = urlopen(url).read()
 
-try:
-    with open('last_response.txt', 'r') as f:
-        last_response = f.read()
-except FileNotFoundError:
-    print('File not yet created')
+    try:
+        with open('last_response.txt', 'r', encoding='ISO-8859-1') as f:
+            last_response = f.read()
+    except FileNotFoundError:
+        print('File not yet created')
 
-response_soup = BeautifulSoup(current_response, 'html.parser')
+    response_soup = BeautifulSoup(current_response, 'html.parser')
 
-ticket_info = response_soup.find_all('p', class_='banner__preamble')[1]
+    ticket_info = response_soup.find_all('p', class_='banner__preamble')[1]
 
-if str(ticket_info) != last_response:
-    message = client.messages.create(
-        body='Ticket info has been updated', from_='+18064041704', to='+35699059592')
-    print(message.sid)
-    with open('last_response.txt', 'w') as f:
-        f.write(str(ticket_info))
-else:
-    print('No changes')
+    if str(ticket_info) != last_response:
+        message = client.messages.create(
+            body='Ticket info has been updated', from_='+18064041704', to='+35699059592')
+        print(message.sid)
+        with open('last_response.txt', 'w', encoding='ISO-8859-1') as f:
+            f.write(str(ticket_info))
+    else:
+        print('No changes')
